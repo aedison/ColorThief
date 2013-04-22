add81a41b02b88ed5b059a0081819c9d6a7435f1
Kevin Tabb <K128kevin@gmail.com>
2013-04-22 12:46:37 -0400

    Added a camera view controller.
    Should only show what the camera sees.  Doesn't take picture yet.

---
+++ ColorThief/ColorThief/CameraViewController.h
@@ -0,0 +1,14 @@
+//
+//  CameraViewController.h
+//  ColorThief
+//
+//  Created by Kevin Tabb on 4/16/13.
+//  Copyright (c) 2013 Alex Edison. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CameraViewController : UIViewController
+- (IBAction)capPress:(id)sender;
+
+@end
---
+++ ColorThief/ColorThief/CameraViewController.m
@@ -0,0 +1,74 @@
+//
+//  CameraViewController.m
+//  ColorThief
+//
+//  Created by Kevin Tabb on 4/16/13.
+//  Copyright (c) 2013 Alex Edison. All rights reserved.
+//
+
+#import "CameraViewController.h"
+
+@interface CameraViewController () <UIImagePickerControllerDelegate>
+
+@end
+
+@implementation CameraViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self)
+    {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+    // Create a bool variable "camera" and call isSourceTypeAvailable to see if camera exists on device
+    BOOL camera = [UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera];
+    
+    // If there is a camera, then display the world throught the viewfinder
+    if(camera)
+    {
+        UIImagePickerController *picker = [[UIImagePickerController alloc] init];
+        
+        // Since I'm not actually taking a picture, is a delegate function necessary?
+        picker.delegate = self;
+        
+        picker.sourceType = UIImagePickerControllerSourceTypeCamera;
+        [self presentModalViewController:picker animated:YES];
+        
+        NSLog(@"Camera is available");
+    }
+    
+    // Otherwise, do nothing.
+    else
+    {
+        NSLog(@"No camera available");
+    }
+}
+
+
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+- (IBAction)capPress:(id)sender
+{
+
+}
+
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info
+{
+    
+}
+
+@end
